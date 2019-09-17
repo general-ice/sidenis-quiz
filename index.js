5ca2eab5e4c29dcfbd3b0f49d563b14791b5dd61
Mozdukov Vyacheslav <vyacheslav.mozdukov@quantumsoft.ru>
2019-09-17 18:20:21 +0700

    remove useless and optimize code

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-(() => {
-    class VirtualDOM {
+class VirtualDOM {
 
-        create(tagName, attributes, children) {
-            return {
-                tagName,
-                attributes,
-                children
-            };
-        }
+    create(tagName, attributes, children) {
+        return {
+            tagName,
+            attributes,
+            children
+        };
+    }
 
-        update(node, vNode) {
-            this.__updateElement(node,vNode)
-        }
+    update(node, vNode) {
+        this.__updateElement(node,vNode)
+    }
 
-        __updateElement(node, vNode) {
-            const nodeChild = [...node.children];
-            if (this.__isEqualElement(node, vNode) && nodeChild.length) {
-                const vNodeChild = vNode.children;
-                vNodeChild.forEach((vNode, i) => {
-                    this.__updateElement(nodeChild[i], vNode)
-                }, []);
-            } else {
-                const necessaryEl = this.__createElement(vNode);
-                node.parentNode.append(necessaryEl);
-                node.remove();
-            }
-        }
-
-        __combineChildren(children) {
-            const childBox = document.createDocumentFragment();
-            children.forEach(el => {
-                const child = this.__createElement(el);
-                childBox.appendChild(child);
-            });
-            return childBox;
-        }
-
-        __isEqualElement(node, vNode) {
-            if (this.__isEqualName(node.tagName, vNode.tagName)) {
-                return this.__isEqualAttr({ ...node.attributes }, vNode.attributes);
-            }
-
-            return false;
-        }
-
-        __isEqualAttr(attrMap1, attrMap2) {
-            if (!(!!attrMap1 && !!attrMap2)) {
-                return true;
-            }
-
-            if (!(!!attrMap1 || !!attrMap2)) {
-                return false;
-            }
-
-            const attrList1 = Object.keys(attrMap1);
-            const attrList2 = Object.keys(attrMap2);
-            if (attrList1.length !== attrList2.length) {
-                return false;
-            }
-
-            return !attrList1.some(attr1 => {
-                return attrMap1[attr1] !== attrMap2[attr1];
-            });
-        }
-
-        __isEqualName(s1, s2) {
-            return s1.toLowerCase() === s2.toLowerCase();
-        }
-
-        __createElement(node) {
-            if (typeof node === 'string') {
-                return document.createTextNode(node)
-            } else {
-                const {tagName, attributes, children} = node;
-                const root = document.createElement(tagName);
-                const styledRoot = !attributes ? root : this.__setAttributes(root, {...attributes});
-                children && children.length && styledRoot.appendChild(this.__combineChildren(children));
-                return root;
-            }
-        }
-
-        __setAttributes(el, attributes) {
-            for (const attr in attributes) {
-                el.setAttribute(attr, attributes[attr]);
-            }
-            return el;
+    __updateElement(node, vNode) {
+        const nodeChild = Array.from(node.children);
+        if (this.__isEqualElement(node, vNode) && nodeChild.length) {
+            const vNodeChild = vNode.children;
+            vNodeChild.forEach((vNode, i) => {
+                this.__updateElement(nodeChild[i], vNode)
+            }, []);
+        } else {
+            const necessaryEl = this.__createElement(vNode);
+            node.parentNode.append(necessaryEl);
+            node.remove();
         }
     }
 
+    __combineChildren(children) {
+        const childBox = document.createDocumentFragment();
+        children.forEach(el => {
+            const child = this.__createElement(el);
+            childBox.appendChild(child);
+        });
+        return childBox;
+    }
+
+    __isEqualElement(node, vNode) {
+        if (this.__isEqualName(node.tagName, vNode.tagName)) {
+            return this.__isEqualAttr(Object.assign({}, node.attributes), vNode.attributes);
+        }
+
+        return false;
+    }
+
+    __isEqualAttr(attrMap1, attrMap2) {
+        if (!(!!attrMap1 && !!attrMap2)) {
+            return true;
+        }
+
+        if (!(!!attrMap1 || !!attrMap2)) {
+            return false;
+        }
+
+        const attrList1 = Object.keys(attrMap1);
+        const attrList2 = Object.keys(attrMap2);
+        if (attrList1.length !== attrList2.length) {
+            return false;
+        }
+
+        return !attrList1.some(attr1 => {
+            return attrMap1[attr1] !== attrMap2[attr1];
+        });
+    }
+
+    __isEqualName(s1, s2) {
+        return s1.toLowerCase() === s2.toLowerCase();
+    }
+
+    __createElement(node) {
+        if (node.length) {
+            return document.createTextNode(node)
+        } else {
+            const {tagName, attributes, children} = node;
+            const root = document.createElement(tagName);
+            const styledRoot = !attributes ? root : this.__setAttributes(root, Object.assign({}, attributes));
+            styledRoot.appendChild(this.__combineChildren(children));
+            return root;
+        }
+    }
+
+    __setAttributes(el, attributes) {
+        for (const attr in attributes) {
+            el.setAttribute(attr, attributes[attr]);
+        }
+        return el;
+    }
+}
+
+const test = () => {
     const rootNode = document.createElement("DIV");
     document.body.appendChild(rootNode);
 
@@ -115,4 +115,6 @@
 
 
     virtualDOM.update(rootNode, rootVNode);
-})();
+};
+
+test();
